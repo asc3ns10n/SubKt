af2daaff422140854e6e6b3e4b54d5c488275a66
asc3ns10n <32691560+asc3ns10n@users.noreply.github.com>
2024-08-05 14:38:36 -0400

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ import java.net.URL
 import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.9.10"
     `java-gradle-plugin`
-    id("org.jetbrains.dokka") version "0.9.18"
+    id("org.jetbrains.dokka") version "1.8.20"
 }
 
 gradlePlugin {
@@ -41,25 +41,25 @@ sourceSets {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8", "1.3.72"))
-    implementation(kotlin("reflect", "1.3.72"))
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.3.7")
+    implementation(kotlin("stdlib-jdk8", "1.9.10"))
+    implementation(kotlin("reflect", "1.9.10"))
+    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core", "1.7.2")
     implementation(gradleApi())
     implementation(gradleKotlinDsl())
-    implementation("org.apache.velocity", "velocity-engine-core", "2.2")
-    implementation("org.apache.pdfbox", "fontbox", "2.0.21")
+    implementation("org.apache.velocity", "velocity-engine-core", "2.3")
+    implementation("org.apache.pdfbox", "fontbox", "2.0.29")
 
-    implementation("com.google.code.gson", "gson", "2.8.6")
-    implementation("io.ktor", "ktor-client-cio", "1.3.2")
-    implementation("io.ktor", "ktor-client-gson", "1.3.2")
-    implementation("io.ktor", "ktor-client-auth-jvm", "1.3.2")
-    implementation("commons-net", "commons-net", "3.6")
-    implementation("commons-io", "commons-io", "2.7")
+    implementation("com.google.code.gson", "gson", "2.10.1")
+    implementation("io.ktor", "ktor-client-cio", "2.3.1")
+    implementation("io.ktor", "ktor-client-gson", "2.3.1")
+    implementation("io.ktor", "ktor-client-auth-jvm", "2.3.1")
+    implementation("commons-net", "commons-net", "3.9.0")
+    implementation("commons-io", "commons-io", "2.13.0")
     implementation("com.jcraft", "jsch", "0.1.55")
     // ttorrent dependencies
-    implementation("org.slf4j", "slf4j-log4j12", "1.6.4")
-    implementation("org.jetbrains", "annotations", "16.0.2")
-    implementation("commons-codec", "commons-codec", "1.14")
+    implementation("org.slf4j", "slf4j-log4j12", "2.0.7")
+    implementation("org.jetbrains", "annotations", "24.0.1")
+    implementation("commons-codec", "commons-codec", "1.16.0")
 
     "samplesImplementation"(sourceSets["main"].output)
 }
@@ -91,7 +91,7 @@ tasks {
         }
 
         externalDocumentationLink {
-            url = URL("https://velocity.apache.org/engine/2.2/apidocs/")
+            url = URL("https://velocity.apache.org/engine/2.3/apidocs/")
         }
 
         packageOptions/*perPackageOption*/ {
